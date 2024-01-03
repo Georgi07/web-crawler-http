6db85c377ae849c56d18fe74b97f2c92c889d28e
Georgi07 <georgigeorgiev161@gmail.com>
2024-01-03 09:34:53 +0200

    getURLsFromHTML function and tests

--- craw.js
+++ craw.js
@@ -1,15 +1,46 @@
-function normalizeURL(url) {
-    const urlObj = new URL(url)
-    console.log(urlObj)
-    const hostPath = `${urlObj.hostname}${urlObj.pathname}`
-    // .slice(-1) in js this returns the last character of string
-    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
-         return hostPath.slice(0, -1)
+// it gives access to dom api's
+const { JSDOM } = require("jsdom");
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      //relative url
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`error with relative url: ${err.message}`);
+      }
+    } else {
+      //absolute url
+      try {
+        const urlObj = new URL(linkElement.href);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`error with absolute url: ${err.message}`);
+      }
     }
-    return hostPath
+  }
+  return urls;
+}
+
+function normalizeURL(url) {
+  const urlObj = new URL(url);
+  console.log(urlObj);
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+  // .slice(-1) in js this returns the last character of string
+  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+    return hostPath.slice(0, -1);
+  }
+  return hostPath;
 }
 
 // This will make 'normalizeURL' function available to other js files
 module.exports = {
-    normalizeURL
-}
+  normalizeURL,
+  getURLsFromHTML,
+};
--- craw.test.js
+++ craw.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} = require('./craw')
+const {normalizeURL, getURLsFromHTML} = require('./craw')
 const {test, expect} = require('@jest/globals')
 
 // Top level test function
@@ -27,5 +27,62 @@ test('normalizeURL strip http', () => {
     const input = 'https://blog.boot.dev/path/'
     const actual = normalizeURL(input)
     const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML absolute', () => {
+    const inputHTMLBoody = `
+     <html>
+      <body>
+       <a href="https://blog.boot.dev/path">Boot.dev Blog</a>
+      </body>
+     </html> 
+    `
+    const inputBaseURL = "https://blog.boot.dev/path"
+    const actual = getURLsFromHTML(inputHTMLBoody, inputBaseURL)
+    const expected = ["https://blog.boot.dev/path"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML relative', () => {
+    const inputHTMLBoody = `
+     <html>
+      <body>
+       <a href="/path/">Boot.dev Blog</a>
+      </body>
+     </html> 
+    `
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTMLBoody, inputBaseURL)
+    const expected = ["https://blog.boot.dev/path/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML both absolute and relative', () => {
+    const inputHTMLBoody = `
+     <html>
+      <body>
+       <a href="/path1/">Boot.dev Blog path one</a>
+       <a href="https://blog.boot.dev/path2/">Boot.dev Blog path two</a>
+      </body>
+     </html> 
+    `
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTMLBoody, inputBaseURL)
+    const expected = ["https://blog.boot.dev/path1/", "https://blog.boot.dev/path2/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML invalid', () => {
+    const inputHTMLBoody = `
+     <html>
+      <body>
+       <a href="invalid">Invalid URL</a>
+      </body>
+     </html> 
+    `
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTMLBoody, inputBaseURL)
+    const expected = []
     expect(actual).toEqual(expected)
 })
